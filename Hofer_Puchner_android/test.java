bac2fd5dc33bb0e71856d08ac3c2ddaa429bea25
Simon <puchner.simon98@gmail.com>
2021-05-15 15:49:26 +0200

    insert outprint in methode

--- Hofer_Puchner_android/test.java
+++ Hofer_Puchner_android/test.java
@@ -1,5 +1,7 @@
 <<<<<<< HEAD
-public void thisIsANewMethod() { }
+public void thisIsANewMethod() { 
+System.out.println(x);
+}
 =======
 String x = "This is a Test";
 >>>>>>> 23121e6ea1d8afe045bf3365cfd2982ccb68bfae

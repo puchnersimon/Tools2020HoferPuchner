804887ea3e9dbfffab1efdf0ca968b3c57faca80
Fabian Hofer <s2010237027@fhooe.at>
2021-05-15 15:54:12 +0200

    Member 1 added test.java

--- Hofer_Puchner_android/test.java
+++ Hofer_Puchner_android/test.java
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
-public void thisIsANewMethod() { }
+public void thisIsANewMethod() {
+String y = "This Member1"; 
+}
 =======
 String x = "This is a Test";
 >>>>>>> 23121e6ea1d8afe045bf3365cfd2982ccb68bfae
+
+
